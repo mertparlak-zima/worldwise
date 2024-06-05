ed8c40127c73e25b49d4c0d0b5e27e71e39a3743
Mert <mertparlak123@hotmail.com>
2024-06-05 11:57:19 +0300

    chore: Update Map component to use react-leaflet and add markers for cities

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,14 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
-import { mapContainer } from "./Map.module.css";
+import { mapContainer, map } from "./Map.module.css";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import { useState } from "react";
+import "leaflet/dist/leaflet.css";
+import { useCities } from "../../contexts/CitiesContext";
 
 export default function Map() {
   const [searchParams, setSearchParams] = useSearchParams();
+  const [mapPosition, setMapPosition] = useState([40, 0]);
+  const { cities } = useCities();
 
   const navigate = useNavigate();
 
@@ -11,7 +17,23 @@ export default function Map() {
 
   return (
     <div onClick={() => navigate("form")} className={mapContainer}>
-      maps
+      <MapContainer className={map} center={mapPosition} zoom={13}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => {
+          const { lat, lng } = city.position;
+          const position = [lat, lng];
+          return (
+            <Marker position={position} key={city.id}>
+              <Popup>
+                <span>{city.emoji}</span> <span>{city.cityName}</span>
+              </Popup>
+            </Marker>
+          );
+        })}
+      </MapContainer>
     </div>
   );
 }
